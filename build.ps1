df8f2f871dbfa1a2620a621fd076bcfb51d1bb31
aldelaro5 <aldelaro5@gmail.com>
2023-08-22 21:20:33 -0400

    Add Debug build configs and cleanup the csproj

--- build.ps1
+++ build.ps1
@@ -1,3 +1,10 @@
-dotnet build .\src\UniverseLib.sln -c Release_IL2CPP
+$BuildType = $args[0]
+if ($BuildType -cne "Release" -and $BuildType -cne "Debug")
+{
+  Write-Output "$BuildType must be ""Release"" or ""Debug""" 
+  return -1
+}
 
-dotnet build .\src\UniverseLib.sln -c Release_Mono
+dotnet build .\src\UniverseLib.sln -c "${BuildType}_IL2CPP"
+
+dotnet build .\src\UniverseLib.sln -c "${BuildType}_Mono"
